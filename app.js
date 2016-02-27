52ceae78bdf325a362a67c45d54239865c59202b
fadyazmy <fadifessa@gmail.com>
2016-02-27 07:33:57 -0500

    get account info and wallet info

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ var client = new Client({
   'baseApiUri': 'https://api.sandbox.coinbase.com/v2/',
   'tokenUri': 'https://api.sandbox.coinbase.com/oauth/token'
 });
+
+
+client.getAccounts({}, function(err, accounts) {
+  accounts.forEach(function(acct) {
+    console.log(acct.name + ': ' + acct.balance.amount + ' ' + acct.balance.currency);
+    acct.getTransactions(null, function(err, txns) {
+      txns.forEach(function(txn) {
+        console.log('txn: ' + txn.id);
+      });
+    });
+  });
+});
+
+
+// refresh the account
+client.getAccount(primaryAccount.id, function(err, acct) {
+  console.log(acct.name + ': ' + acct.balance.amount + ' ' + acct.balance.currency);
+});
